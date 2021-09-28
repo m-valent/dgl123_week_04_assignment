1ddb2f46f4071a63094faa5eedf9d870330d8720
Misko Valent <55299389+m-valent@users.noreply.github.com>
2021-09-28 13:45:21 -0700

    Ready myself for assignment
    blank slate

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
 
-$age = 11;
-$height = 5;
-
-if ($height >= 5 && $age > 10) {
-    echo 'You can ride the roll coaster, enjoy!';
-} elseif ($height >= 5) {
-    echo 'Sorry you are not old enough to ride the roller coaster.';
-} else {
-    echo 'Sorry you are not tall enough to ride the roller coaster.';
-}
 
 ?>
