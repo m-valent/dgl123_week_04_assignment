3c7570271203605637b878e8a6e74268133fb538
Misko Valent <55299389+m-valent@users.noreply.github.com>
2021-09-28 13:24:05 -0700

    Played with Boolean statements

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
+$age = 9;
 $height = 5;
 
-if ($height >= 5) {
-echo 'Welcome to the supercoaster!';
-}
+// if ($height >= 5) {
+// echo 'Welcome to the supercoaster!';
+// }
 
+var_dump($height >= 5 || $age > 10);
 
 ?>
