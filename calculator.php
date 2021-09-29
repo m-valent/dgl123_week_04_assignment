7337c1bac78e7bd9090037033647f0ec38ac08c4
Misko Valent <55299389+m-valent@users.noreply.github.com>
2021-09-28 19:06:48 -0700

    Setting up page to match html sample

---
+++ calculator.php
@@ -0,0 +1,11 @@
+<h1>Addition Only Calculator</h1>
+
+<form action="">
+    <label for="first">First Name</label>
+    <input type="number" name="first" id="first" value="required">
+    <br>
+    <label for="second">Second Number</label>
+    <input type="number" name="second" id="second" value="required">
+    <br>
+    <input type="submit" value="Calculate SUM">
+</form>
