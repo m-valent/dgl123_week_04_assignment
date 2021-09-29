d9e7cad07cd4949be0d86ad07c3246d9a353221e
Misko Valent <55299389+m-valent@users.noreply.github.com>
2021-09-28 19:19:43 -0700

    Added title, info, and html

--- calculator.php
+++ calculator.php
@@ -1,11 +1,44 @@
-<h1>Addition Only Calculator</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<form action="">
-    <label for="first">First Name</label>
-    <input type="number" name="first" id="first" value="required">
-    <br>
-    <label for="second">Second Number</label>
-    <input type="number" name="second" id="second" value="required">
-    <br>
-    <input type="submit" value="Calculate SUM">
-</form>
+<?php
+/*
+Title:      Week 04 Assignment
+
+Student:    Misko Valent
+Due Date:   Sept 29, 2021
+Filename:   calculator.php
+
+Class:      DGL 123 - Introduction to PHP
+Semester:   2021 Fall North Island College
+Instructor: Brad Best
+
+NOTE:  I know I do not require the extra HTML code, but for now am including it
+unless you wish to just focus on the PHP and ignore the added HTML?
+*/
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DGL 123 - Week 04 - Calculator</title>
+</head>
+
+<body>
+
+    <h1>Addition ONLY Calculator</h1>
+
+    <form action="">
+        <label for="first">First Name</label>
+        <input type="number" name="first" id="first" value="required">
+        <br>
+        <label for="second">Second Number</label>
+        <input type="number" name="second" id="second" value="required">
+        <br>
+        <input type="submit" value="Calculate SUM">
+    </form>
+
+</body>
+
+</html>
